9208677a5619c35ea633082f5f496d5052a341b3
Domara Hawil <domara.hawil@hotmail.com>
2022-11-16 14:58:24 -0500

    snakeclient

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require("net");
+const connect = function () {
+  
+  const conn = net.createConnection({
+    host: '10.0.0.24',
+    port: '50541'
+    
+  })
+  conn.on("connect", () => {
+    console.log("You have succesfully connected")
+  })
+  
+  conn.on("data", (data) => {
+      console.log(data)
+    })
+    
+    conn.setEncoding("utf8");
+    
+    return conn;
+  };
+  
+  const { IP, PORT } = require("./constants");
+  
+  module.exports = {connect}
